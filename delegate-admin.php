e73387caa6c09ce6a257aaf9482737ec09db1b2b
boston <bob.t.allen@gmail.com>
2020-01-07 09:12:26 -0800

    Update delegate-admin.php

--- delegate-admin.php
+++ delegate-admin.php
@@ -1,4 +1,12 @@
 <?php
+/*!
+ * @overview superintuitive - a drag and drop webapp builder with point and click attribute and style editing.
+ * @copyright Copyright (c) 2020 Robert Allen
+ * @license   Licensed under GPLv2 license
+ *            See https://github.com/disscombobilated/SuperIntuitive/blob/master/LICENSE
+ * @version   v0.8
+ */
+
 session_start();
 require_once 'core/Tools.php';
 Tools::Autoload('root');
