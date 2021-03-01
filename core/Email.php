e5b81abd5dbf8d33bf2d64345b7d7ba2376676d0
Bob Allen <bob.t.allen@gmail.com>
2021-03-01 10:53:55 -0500

    email start

--- core/Email.php
+++ core/Email.php
@@ -13,20 +13,46 @@ class Email {
 
 	public function Send($post){
 		//the simplist email only has a to address to send to and it must be an array.
-		$to = "";
-		$subject = " ";
-		$message = " ";
+		if(isset($post['to']) && isset($post['message']) ){
 
-		if(isset($post["to"])){
-			$to = "";
-			for($post["to"] as $eachto){
-				$to.=$eachto.", ";
+			$to = $post['to'];
+
+			$subject = '';
+			if(isset($post['subject'])){
+				$subject = $post['subject'];
 			}
-			$to = rtrim($to, ', ');
 
+			$message = '';
+			if(isset($post['message'])){
+				$message = $post['message'];
+			}
 
+ 
+			// Additional headers
+			// To send HTML mail, the Content-type header must be set
+			$headers[] = 'MIME-Version: 1.0';
+			$headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
-		}
+			//this can't come from the client. it must be known by the server
+			//If the email client is setup, most of the time this will be the logged in users domain email. 
+			//It could also be the notification email. 
+			//TODO develop logic to handle email  
+			$from = ""; 
+			if(isset($from)){
+				$headers[] = 'From: '.$from;
+			}
+
+			if(isset($post['cc'])){
+				$headers[] = 'Cc: '.$post['cc'];
+			}
+
+			if(isset($post['bcc'])){
+				$headers[] = 'Bcc: '.$post['bcc'];
+			}
+
+			// Mail it
+			mail($to, $subject, $message, implode("\r\n", $headers));
+	}
 
 	}
 
